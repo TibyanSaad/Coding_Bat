987f4368d4f2f5d7fc3339d0cf170af164f1e609
TibyanSaad <saad.tibyan02@gmail.com>
2026-02-16 08:08:36 +0400

    added hasTeen

---
+++ hasTeen.java
@@ -0,0 +1,12 @@
+public class hasTeen {
+    public boolean hasTeen(int a, int b, int c) {
+        if((a>=13&& a<=19) || (b>=13&& b<=19) ||(c>=13&& c<=19)){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args){
+        hasTeen teen = new hasTeen();
+        System.out.println(teen.hasTeen(15,72,19));
+    }
+}
