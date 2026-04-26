c2c48dba57ccde0c8eeeab7962f7c04e7bcf57d5
TibyanSaad <saad.tibyan02@gmail.com>
2026-04-26 08:36:15 +0400

    added task

--- MapOne/MapOne.java
+++ MapOne/MapOne.java
@@ -11,4 +11,11 @@ public class MapOne {
         return map;
     }
 
+    public Map<String, String> mapBully(Map<String, String> map) {
+        if (map.containsKey("a")) {
+            map.put("b", map.get("a"));
+            map.put("a", "");
+        }
+        return map;
+    }
 }
