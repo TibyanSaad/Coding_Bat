82d9231daf63c042c6d797c8fd48a1ac9549c517
TibyanSaad <saad.tibyan02@gmail.com>
2026-04-26 08:37:07 +0400

    added task

--- MapOne/MapOne.java
+++ MapOne/MapOne.java
@@ -27,4 +27,12 @@ public class MapOne {
         }
         return map;
     }
+
+    public Map<String, String> topping1(Map<String, String> map) {
+        if (map.containsKey("ice cream")) {
+            map.put("ice cream", "cherry");
+        }
+        map.put("bread", "butter");
+        return map;
+    }
 }
