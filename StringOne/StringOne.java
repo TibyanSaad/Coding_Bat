eae4cb4d77ba231e15dbac75ec6ec542ca7c2d97
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 16:31:45 +0400

    added task

--- StringOne/StringOne.java
+++ StringOne/StringOne.java
@@ -165,4 +165,13 @@ public class StringOne {
         }
         return "@@";
     }
+
+    public String conCat(String a, String b) {
+        if (a.length() > 0 && b.length() > 0) {
+            if (a.charAt(a.length() - 1) == b.charAt(0)) {
+                return "" + a.substring(0, a.length() - 1) + b;
+            }
+        }
+        return a + b;
+    }
 }
