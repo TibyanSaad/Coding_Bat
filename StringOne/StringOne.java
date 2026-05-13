bff2291f99b31c40a7572edaf48f7ff7a8938268
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 16:29:25 +0400

    added task

--- StringOne/StringOne.java
+++ StringOne/StringOne.java
@@ -59,4 +59,15 @@ public class StringOne {
         }
         return str;
     }
+
+    public String right2(String str) {
+        if (str.length() > 2) {
+            String start = str.substring(0, str.length() - 2);
+            String end = str.substring(str.length() - 2);
+            return end + start;
+        }
+        return str;
+    }
+
+
 }
