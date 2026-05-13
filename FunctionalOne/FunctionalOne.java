b6b19f5d255b67ae57e0b439bfc85a9daeeb16c5
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 16:19:17 +0400

    added task

--- FunctionalOne/FunctionalOne.java
+++ FunctionalOne/FunctionalOne.java
@@ -23,5 +23,10 @@ public class FunctionalOne {
         return strings;
     }
 
+    public List<String> moreY(List<String> strings) {
+        strings.replaceAll(s -> "y" + s + "y");
+        return strings;
+    }
+
 
 }
