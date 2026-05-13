050df0e6cf9ec813124ff0dbf75d0532fc61823e
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 16:27:53 +0400

    added task

--- StringOne/StringOne.java
+++ StringOne/StringOne.java
@@ -13,4 +13,8 @@ public class StringOne {
         return "<" + tag + ">" + word + "</" + tag + ">";
     }
 
+    public String makeOutWord(String out, String word) {
+        return out.substring(0, 2) + word + out.substring(2);
+    }
+
 }
