ed978a202811bd2eb5489883b4bdd96309678ab5
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 16:07:00 +0400

    added task

--- MapTwo/MapTwo.java
+++ MapTwo/MapTwo.java
@@ -1,4 +1,13 @@
 package MapTwo;
 
+import java.util.*;
+
 public class MapTwo {
+    public Map<String, Integer> word0(String[] strings) {
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for (String key: strings){
+            map.put(key,0);
+        }return map;
+    }
+
 }
