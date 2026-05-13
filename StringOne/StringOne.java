b2c2d94d2dc02577749f1af5380cc242c25d2e4d
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 16:27:34 +0400

    added task

--- StringOne/StringOne.java
+++ StringOne/StringOne.java
@@ -4,4 +4,9 @@ public class StringOne {
     public String helloName(String name) {
         return ("Hello " + name + "!");
     }
+
+    public String makeAbba(String a, String b) {
+        return a + b + b + a;
+    }
+
 }
