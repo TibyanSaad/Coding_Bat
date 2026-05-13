454a8367574da69b8615d5b078972148fb9c32eb
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 16:17:21 +0400

    added task

--- FunctionalOne/FunctionalOne.java
+++ FunctionalOne/FunctionalOne.java
@@ -1,4 +1,11 @@
 package FunctionalOne;
 
+import java.util.List;
+
 public class FunctionalOne {
+    public List<Integer> doubling(List<Integer> nums) {
+        nums.replaceAll(n -> n * 2);
+        return nums;
+    }
+
 }
