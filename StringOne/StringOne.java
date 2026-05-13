fde4f09a95fee347b4f1b4e30a767ab28834bfc2
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 16:32:42 +0400

    added task

--- StringOne/StringOne.java
+++ StringOne/StringOne.java
@@ -206,4 +206,11 @@ public class StringOne {
         }
         return false;
     }
+
+    public String minCat(String a, String b) {
+        int minLen = Math.min(a.length(), b.length());
+        String aTrimmed = a.substring(a.length() - minLen);
+        String bTrimmed = b.substring(b.length() - minLen);
+        return aTrimmed + bTrimmed;
+    }
 }
