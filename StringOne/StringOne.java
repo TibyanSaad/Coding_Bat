b1c96a935ceb674fda614d8f4d4584f6c8d78091
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 16:29:01 +0400

    added task

--- StringOne/StringOne.java
+++ StringOne/StringOne.java
@@ -46,4 +46,10 @@ public class StringOne {
         }
         return a + b + a;
     }
+
+    public String nonStart(String a, String b) {
+        return a.substring(1) + b.substring(1);
+    }
+
+
 }
