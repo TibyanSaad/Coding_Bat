3fdfb2c7960c4da1fc3bed3f042f0c66aec8d5c3
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 16:31:33 +0400

    added task

--- StringOne/StringOne.java
+++ StringOne/StringOne.java
@@ -154,4 +154,15 @@ public class StringOne {
         }
         return at;
     }
+
+    public String lastChars(String a, String b) {
+        if (a.length() >= 1 && b.length() >= 1) {
+            return "" + a.charAt(0) + b.charAt(b.length() - 1);
+        } else if (a.length() < 1 && b.length() >= 1) {
+            return "" + '@' + b.charAt(b.length() - 1);
+        } else if (a.length() >= 1 && b.length() < 1) {
+            return "" + a.charAt(0) + '@';
+        }
+        return "@@";
+    }
 }
