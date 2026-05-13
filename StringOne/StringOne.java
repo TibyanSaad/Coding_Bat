53f6eddaf0fe32b1f9d32d5af72d94bcc61ae355
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 16:33:22 +0400

    added task

--- StringOne/StringOne.java
+++ StringOne/StringOne.java
@@ -234,4 +234,18 @@ public class StringOne {
         }
         return str;
     }
+
+    public String deFront(String str) {
+        String result = "";
+        if (str.length() >= 1 && str.charAt(0) == 'a') {
+            result += "a";
+        }
+        if (str.length() >= 2 && str.charAt(1) == 'b') {
+            result += "b";
+        }
+        if (str.length() > 2) {
+            result += str.substring(2);
+        }
+        return result;
+    }
 }
