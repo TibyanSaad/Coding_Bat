cc05fb885fed46c5ce51435e3d34d609d3bd70d0
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 16:09:50 +0400

    reformat code

--- MapTwo/MapTwo.java
+++ MapTwo/MapTwo.java
@@ -5,32 +5,35 @@ import java.util.*;
 public class MapTwo {
     public Map<String, Integer> word0(String[] strings) {
         Map<String, Integer> map = new HashMap<String, Integer>();
-        for (String key: strings){
-            map.put(key,0);
-        }return map;
+        for (String key : strings) {
+            map.put(key, 0);
+        }
+        return map;
     }
 
     public Map<String, Integer> wordLen(String[] strings) {
         Map<String, Integer> map = new HashMap<String, Integer>();
-        for (String key: strings){
-            map.put(key,key.length());
-        }return map;
+        for (String key : strings) {
+            map.put(key, key.length());
+        }
+        return map;
     }
 
     public Map<String, String> pairs(String[] strings) {
         Map<String, String> map = new HashMap<String, String>();
-        for (String key: strings){
-            map.put(String.valueOf(key.charAt(0)),String.valueOf(key.charAt(key.length()-1)));
-        }return map;
+        for (String key : strings) {
+            map.put(String.valueOf(key.charAt(0)), String.valueOf(key.charAt(key.length() - 1)));
+        }
+        return map;
     }
 
     public Map<String, Integer> wordCount(String[] strings) {
         Map<String, Integer> map = new HashMap<String, Integer>();
-        for (String key: strings){
+        for (String key : strings) {
             if (map.containsKey(key)) {
-                map.put(key,map.get(key)+1);
+                map.put(key, map.get(key) + 1);
             } else {
-                map.put(key,1);
+                map.put(key, 1);
             }
         }
         return map;
@@ -38,14 +41,15 @@ public class MapTwo {
 
     public Map<String, String> firstChar(String[] strings) {
         Map<String, String> map = new HashMap<String, String>();
-        for (String key: strings){
+        for (String key : strings) {
             String first = String.valueOf(key.charAt(0));
-            if (map.containsKey(first)){
-                map.put(first, map.get(first)+key);
-            }else {
+            if (map.containsKey(first)) {
+                map.put(first, map.get(first) + key);
+            } else {
                 map.put(first, key);
             }
-        }return map;
+        }
+        return map;
 
     }
 
@@ -86,7 +90,8 @@ public class MapTwo {
             } else {
                 map.put(firstChar, i);
             }
-        } return strings;
+        }
+        return strings;
     }
 
     public String[] firstSwap(String[] strings) {
@@ -102,6 +107,7 @@ public class MapTwo {
             } else {
                 map.put(firstChar, i);
             }
-        }return strings;
+        }
+        return strings;
     }
 }
