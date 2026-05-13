ec12893284bf411b6127b82eb64980496763149e
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 16:09:29 +0400

    added task

--- MapTwo/MapTwo.java
+++ MapTwo/MapTwo.java
@@ -89,5 +89,19 @@ public class MapTwo {
         } return strings;
     }
 
-
+    public String[] firstSwap(String[] strings) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < strings.length; i++) {
+            char firstChar = strings[i].charAt(0);
+            if (map.containsKey(firstChar)) {
+                int prevIndex = map.get(firstChar);
+                String temp = strings[i];
+                strings[i] = strings[prevIndex];
+                strings[prevIndex] = temp;
+                map.remove(firstChar);
+            } else {
+                map.put(firstChar, i);
+            }
+        }return strings;
+    }
 }
