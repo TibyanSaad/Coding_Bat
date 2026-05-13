d612539f466dc0691fcfa57bfd69700305484e94
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 16:30:16 +0400

    added task

--- StringOne/StringOne.java
+++ StringOne/StringOne.java
@@ -95,4 +95,14 @@ public class StringOne {
         return str;
 
     }
+
+    public boolean endsLy(String str) {
+        if (str.length() > 1) {
+            if (str.charAt(str.length() - 1) == 'y'
+                    && str.charAt(str.length() - 2) == 'l') {
+                return true;
+            }
+        }
+        return false;
+    }
 }
